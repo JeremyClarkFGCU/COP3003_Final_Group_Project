a02baf0ff1cae6685c2f9885a80a273397949222
JeremyClarkFGCU <jtclark1825@eagle.fgcu.edu>
2023-04-19 20:45:44 -0400

    Addded menu header

---
+++ COP3003_Group_Project/menu.h
@@ -0,0 +1,79 @@
+// ==============================================
+// | menu.h										|
+// | Authors; Rainier St. Fort, Markendy Pierre	|
+// | Desc: Contains operations for game menus	|
+// ==============================================
+
+#ifndef IN_GAME_MENU
+#define IN_GAME_MENU
+
+#include "function_defs.h"
+#include "classes.h"
+
+
+using namespace std;
+
+
+void select_weapon(Player & p) {
+	cout << "You currently have " << p.get_weapon_name() << " equipped.\n";
+	cout << "Equip one of the following?" << endl;
+}// Menu to select and equip new weapon.
+
+void select_armor(Player & p) {
+	cout << "You currently have " << p.get_armor_name() << " equipped.\n";
+	cout << "Equip one of the following?\n";
+}// Menu to select and equip new armor.
+
+void equipment_menu(Player & p) {
+	system("cls");
+	cout<< "Select Equipment:"<< endl;
+	cout << endl;
+	cout << "1. Weapon" << endl;
+	cout << "2. Armor" << endl;
+	int equipmentChoice = get_integer();
+
+	switch(equipmentChoice){
+		case 1:
+			select_weapon(p);
+			break;
+		case 2:
+			select_armor(p);
+			break;
+		case 0:
+			break;
+	};
+}
+
+void in_game_menu(Player& p) {
+
+	cout << "Menu:" << endl;
+	cout << "1. Character Stats" << endl;
+	cout << "2. Equipment" << endl;
+	cout << "3. Use Items" << endl;
+	cout << "0. Exit" << endl;
+
+	int menuChoice = get_integer();
+
+	switch (menuChoice) {
+	case 1:
+		system("cls");
+		print(p);
+		cout << endl << endl;
+			cout<< "Enter 0 to go back" << endl;
+			int goBack;
+			cin >> goBack;
+		break;
+	case 2:
+		equipment_menu(p);
+		break;
+
+	case 0:
+		break;
+	}
+}
+
+#endif
+
+
+
+
